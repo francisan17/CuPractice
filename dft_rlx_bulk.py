a0533b3bf605ac2cd0d4811849fac612f53748db
Pavel Stishenko <pstishenko@gmail.com>
2024-07-31 12:28:09 +0100

    Cleaned DFT script for bulk; Added DFT script for ST slab

--- dft_rlx_bulk.py
+++ dft_rlx_bulk.py
@@ -18,40 +18,35 @@ print ("sys.argv = ", sys.argv)
 set_aims_command(hpc="hawk", basis_set="light", defaults=2020)
 
 aims_dir=f'/scratch/{os.environ["USER"]}/tmp_aims_{jid}'
-print (f'{aims_dir=}')
 os.mkdir(aims_dir)
 
-n_layers = int(sys.argv[1])
+print (f'{aims_dir=}')
 
-
-socket_calc, fhi_calc = get_aims_and_sockets_calculator(dimensions=0, logfile=f'{aims_dir}/socketio.log')
+sockets_calc, fhi_calc = get_aims_and_sockets_calculator(dimensions=0, logfile=f'{aims_dir}/socketio.log')
 fhi_calc.directory = aims_dir
 
 fhi_calc.parameters.pop("xc")
 fhi_calc.set(override_warning_libxc="true", # <---- necessary !!!
-       #override_warning_libxc="True",
        xc='libxc MGGA_X_MBEEF+GGA_C_PBE_SOL',
        xc_pre=['pbe', '50'],
        spin='none', # if any(init_magmoms) != 0 else 'none',
-       k_grid=(3,3,1),   # to be used in a 3x2 cell
+       k_grid=(3,3,3),   # to be used in a 3x2 cell
        relativistic=('atomic_zora','scalar'),
        #compensate_multipole_errors='True',
-       use_dipole_correction='True',
+       #use_dipole_correction='True',
        compute_forces="true",
-       #compute_analytical_stress="true",
+       compute_analytical_stress="true",
        mixer='pulay',
-       #charge_mix_param=0.05,
        occupation_type='gaussian 0.01',
        #sc_accuracy_etot=1e-5,
        #sc_accuracy_forces=1e-3,
        #sc_accuracy_rho=5e-3,
-       sc_iter_limit=300    )
+       sc_iter_limit=300)
 
 
-bulk = read('Cu2O_rlxdft.xyz')
-
-superslab = STO_FCC111(bulk, n_layers=n_layers, vacuum=10)
-print(f'{superslab=}')
-superslab.calc = socket_calc
-E_slab = superslab.get_potential_energy()
-print(f'{n_layers=} {E_slab=}')
+bulk = cu2o_bulk()
+bulk.set_calculator(sockets_calc)
+ucf = UnitCellFilter(bulk)
+LBFGS(ucf).run(fmax=0.01)
+E_bulk = bulk.get_potential_energy()
+write('Cu2O_rlxdft.xyz', bulk)
